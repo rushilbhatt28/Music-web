317b9dc105a08dd48b9bedf1509694e03adf211d
Rushil Bhatt <rushilbhatt533@gmail.com>
2022-05-08 17:35:55 +0200

    Added Header

--- src/player/Body.js
+++ src/player/Body.js
@@ -1,5 +1,18 @@
 import React from "react";
 import "../assets/Body.css";
-export default function Body() {
-  return <div className="body">Body</div>;
+import Header from "../Header/Header";
+export default function Body({ spotify }) {
+  return (
+    <div className="body">
+      <Header spotify={spotify} />
+      <div className="body__info">
+        <img src="" alt="" />
+        <div className="body__infoText">
+          <strong>Playlist</strong>
+          <h2>Discover weekly</h2>
+          <p>description</p>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/player/Player.js
+++ src/player/Player.js
@@ -3,12 +3,14 @@ import "../assets/Player.css";
 import Body from "./Body";
 import SideBar from "../sideBar/sideBar";
 import Footer from "./Footer";
+import Header from "../Header/Header";
 function Player({ spotify }) {
   return (
     <div className="player">
       <div className="player__body">
         <SideBar />
         <Body />
+        <Header />
       </div>
       <Footer />
     </div>
